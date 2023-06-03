7f863dfd016a92df4af77f627f8d3748165397a4
Youcee <chesnuel08@gmail.com>
2023-06-03 17:30:57 +0200

    edit

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket         = "terraform-storage-789"
-    prefix         = "terraform/state"
-  }
-}
 
 # google_client_config and kubernetes provider must be explicitly specified like the following.
 data "google_client_config" "default" {}
